0da6cb6cf06570299055d4a06935160726c34fa3
nicolas <nickzheng180@gmail.com>
2021-08-13 16:59:40 -0700

    before using loop to discard non-function relations

--- folders/Tools/ExcelReading.py
+++ folders/Tools/ExcelReading.py
@@ -1,17 +1,51 @@
-from openpyxl import Workbook
-from openpyxl import load_workbook
+import pandas as pd
 
-workbook = load_workbook(filename='Automated Scheduling Test Local.xlsx', data_only=True)
+currentSheet_DataFrame = pd.read_excel(
+    open("Automated Scheduling Test Local.xlsx", 'rb'),
+    sheet_name='Panopto Folders to Create')
 
-# print(workbook['Panopto Folders to Create']['G2'].value)
-ws = workbook['Panopto Folders to Create']
-r = 1
-# for row in ws.iter_rows('G{}:G{}'.format(ws.min_row, ws.max_row)):
-#     for cell in row:
-#         print(cell.value)
-#         r += 1
+parent_course_folder_df = currentSheet_DataFrame[['Division Folder', 'Course Level Folder']]
 
-for row in ws.iter_rows(min_row=1, min_col=7, max_row=ws.max_row, max_col=7):
-    for cell in row:
-        print(cell.value, end=" ")
-    print()
+parent_course_folder_df = parent_course_folder_df.dropna()
+parent_course_folder_df.columns = ['ParentName', 'CourseName']
+
+data = []
+prev = None
+
+multipleSharedCourses = [
+    "COMM 101",
+    "COMM 120",
+    "COMM 186",
+    "COMM 220",
+    "COMM 292",
+    "COMM 335",
+    "COMM 386",
+    "COMM 390",
+    "COMM 394",
+    "COMM 395"
+]
+multipleSharedCourses_df = pd.DataFrame(multipleSharedCourses, columns=["CourseName"])
+
+# parent_course_folder_df.isin(multipleSharedCourses_df)
+
+parent_course_folder_df = parent_course_folder_df[~parent_course_folder_df['CourseName'].isin(multipleSharedCourses_df['CourseName'])]
+
+print(parent_course_folder_df)
+
+
+
+
+
+for index, row in parent_course_folder_df.iterrows():
+    if prev != row["CourseName"]:
+        data.append({'ParentName': row["ParentName"], 'CourseName': row["CourseName"]})
+
+    prev = row["CourseName"]
+
+    # print(row["Division Folder"], row["Course Level Folder"])
+
+data_df = pd.DataFrame(data)
+
+
+# print(multipleSharedCourses_df)
+# print(data_df)
