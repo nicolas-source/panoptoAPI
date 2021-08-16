3c61e66aaf290af68254b89ab8b92b13b53d3eb5
nicolas <nickzheng180@gmail.com>
2021-08-16 09:56:31 -0700

    Able to filter out courses with  non-func like relations

--- folders/Tools/ExcelReading.py
+++ folders/Tools/ExcelReading.py
@@ -27,56 +27,17 @@ multipleSharedCourses = [
 ]
 multipleSharedCourses_df = pd.DataFrame(multipleSharedCourses, columns=["CourseName"])
 
-# parent_course_folder_df.isin(multipleSharedCourses_df)
-
-# parent_course_folder_df = parent_course_folder_df[
-#     ~parent_course_folder_df['CourseName'].isin(multipleSharedCourses_df['CourseName'])]
-#
-# print(parent_course_folder_df)
-
-def dictionaryExistInList(key, value, list):
-    if not list:
-        return False, None
-    for item in list:
-        if item[key] == value:
-            return True, item
-
-
-funcData = []
-nonFuncData = []
 
 print("Original\n", parent_course_folder_df)
 
-for index, row in parent_course_folder_df.iterrows():
-    existence, item = dictionaryExistInList("CourseName", row["CourseName"], funcData)
-    if existence:
-        if item is not None:
-            if item["ParentName"] == row["ParentName"]:
-                continue
-            else:
-                nonFuncData.append({'ParentName': row["ParentName"], 'CourseName': row["CourseName"]})
-    else:
-        funcData.append({'ParentName': row["ParentName"], 'CourseName': row["CourseName"]})
+# print(parent_course_folder_df.duplicated(subset=["CourseName"]))
+
+# print("Total non-func relations\n", parent_course_folder_df[parent_course_folder_df.duplicated(subset=["CourseName"])])
+print("Total unique non-func relations\n", parent_course_folder_df[parent_course_folder_df.duplicated(subset=["CourseName"])].drop_duplicates(subset=["CourseName"]))
+print("Total unique non-func relations length\n", len(parent_course_folder_df[parent_course_folder_df.duplicated(subset=["CourseName"])].drop_duplicates(subset=["CourseName"])))
 
 
 
 
 
-funcData_df = pd.DataFrame(funcData)
-nonFuncData_df = pd.DataFrame(nonFuncData)
 
-print(funcData_df)
-print(nonFuncData_df)
-
-# for index, row in parent_course_folder_df.iterrows():
-#     if prev != row["CourseName"]:
-#         data.append({'ParentName': row["ParentName"], 'CourseName': row["CourseName"]})
-#
-#     prev = row["CourseName"]
-#
-#     # print(row["Division Folder"], row["Course Level Folder"])
-#
-# data_df = pd.DataFrame(data)
-
-# print(multipleSharedCourses_df)
-# print(data_df)
