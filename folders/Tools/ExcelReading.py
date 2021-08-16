cb379695d98e11e0872f386be2ca55a7e9cf4a22
nicolas <nickzheng180@gmail.com>
2021-08-16 10:26:39 -0700

    Cleaned list of course and parent folder data completed

--- folders/Tools/ExcelReading.py
+++ folders/Tools/ExcelReading.py
@@ -1,5 +1,6 @@
 import pandas as pd
 
+
 currentSheet_DataFrame = pd.read_excel(
     open("Automated Scheduling Test Local.xlsx", 'rb'),
     sheet_name='Panopto Folders to Create')
@@ -13,19 +14,6 @@ parent_course_folder_df = parent_course_folder_df.drop_duplicates()
 data = []
 prev = None
 
-multipleSharedCourses = [
-    "COMM 101",
-    "COMM 120",
-    "COMM 186",
-    "COMM 220",
-    "COMM 292",
-    "COMM 335",
-    "COMM 386",
-    "COMM 390",
-    "COMM 394",
-    "COMM 395"
-]
-multipleSharedCourses_df = pd.DataFrame(multipleSharedCourses, columns=["CourseName"])
 
 
 print("Original\n", parent_course_folder_df)
@@ -33,11 +21,28 @@ print("Original\n", parent_course_folder_df)
 # print(parent_course_folder_df.duplicated(subset=["CourseName"]))
 
 # print("Total non-func relations\n", parent_course_folder_df[parent_course_folder_df.duplicated(subset=["CourseName"])])
+
+uniqueNonFuncRelations = parent_course_folder_df[parent_course_folder_df.duplicated(subset=["CourseName"])].drop_duplicates(subset=["CourseName"])
 print("Total unique non-func relations\n", parent_course_folder_df[parent_course_folder_df.duplicated(subset=["CourseName"])].drop_duplicates(subset=["CourseName"]))
 print("Total unique non-func relations length\n", len(parent_course_folder_df[parent_course_folder_df.duplicated(subset=["CourseName"])].drop_duplicates(subset=["CourseName"])))
 
 
+print(uniqueNonFuncRelations["CourseName"])
+print(len(uniqueNonFuncRelations["CourseName"]))
 
 
+print(parent_course_folder_df["CourseName"].isin(uniqueNonFuncRelations["CourseName"]))
+print(parent_course_folder_df[~parent_course_folder_df["CourseName"].isin(uniqueNonFuncRelations["CourseName"])])
+filtered_parent_course_folder_df = parent_course_folder_df[~parent_course_folder_df["CourseName"].isin(uniqueNonFuncRelations["CourseName"])]
 
+for index, row in uniqueNonFuncRelations.iterrows():
+    row["ParentName"] = "Program Wide Courses"
 
+print(uniqueNonFuncRelations)
+
+completeMapping = filtered_parent_course_folder_df.append(uniqueNonFuncRelations, ignore_index=True)
+print(completeMapping)
+
+from tabulate import tabulate
+
+print(tabulate(completeMapping, headers = 'keys', tablefmt = 'psql'))
