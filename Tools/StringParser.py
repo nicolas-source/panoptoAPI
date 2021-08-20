b34f904069ea9d6cdb3155b6c0db91664e08801d
nicolas <nickzheng180@gmail.com>
2021-08-20 10:02:10 -0700

    Parser works for test cases of given format

--- Tools/StringParser.py
+++ Tools/StringParser.py
@@ -1,24 +1,12 @@
 import pyparsing as pp
 
-greet = pp.Word(pp.alphas) + "," + pp.Word(pp.alphas) + "!"
 
-
-hello = "Hello, World!"
-print(hello, "->", greet.parseString(hello))
-
-for greeting_str in [
-            "Hello, World!",
-            "Bonjour, Monde!",
-            "Hola, Mundo!",
-            "Hallo, Welt!",
-        ]:
-    # print(greet)
-    greeting = greet.parseString(greeting_str)
-    print(greeting)
-
-greet2 = pp.Word(pp.alphas) + " " + pp.Word(pp.alphas)
+parseFormat = pp.Word(pp.alphas) + pp.Word(pp.alphanums) + \
+         '-' + pp.Word(pp.alphas) + pp.Word(pp.alphanums) + pp.Word(pp.nums) + \
+         '-' + pp.Word(pp.alphanums) + pp.Word(pp.alphas) + pp.Word(pp.printables) + pp.Word(pp.printables)
 
 str1 = "LASR 102 - ARTH 322 001 - 2021W2 on 4/7/2022 (Thu)"
 str2 = "BUCH B313 - GRSJ 224B 002 - 2021W2 on 4/7/2022 (Thu)"
 
-print(greet2.parseString())
+print(parseFormat.parseString(str1))
+print(parseFormat.parseString(str2))
