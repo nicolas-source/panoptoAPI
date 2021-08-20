fb9dd31b620fd7aa38723570ac55529065fd6286
nicolas <nickzheng180@gmail.com>
2021-08-19 19:36:25 -0700

    Reaches 'Scheduled' tab successfully

--- selenium/scraper.py
+++ selenium/scraper.py
@@ -1,5 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+
+import getpass
 
 import os
 from os.path import abspath
@@ -16,11 +19,22 @@ chromedriver = file_dir + "/chromedriver"
 
 # driver = webdriver.Chrome()
 driver = webdriver.Chrome(chromedriver)
-driver.get("http://www.python.org")
-assert "Python" in driver.title
-elem = driver.find_element_by_name("q")
-elem.clear()
-elem.send_keys("pycon")
-elem.send_keys(Keys.RETURN)
-assert "No results found." not in driver.page_source
+
+driver.get("https://ubc.ca.panopto.com/Panopto/Pages/Auth/Login.aspx?ReturnUrl=https%3A%2F%2Fubc.ca.panopto.com%2FPanopto%2FPages%2FSessions%2FList.aspx%23isMyFolder%3Dtrue")
+
+driver.find_element_by_xpath('//*[@id="usernameInput"]').send_keys("nzheng8")
+
+print("Enter Password:\n")
+password = getpass.getpass()
+
+driver.find_element_by_xpath('//*[@id="passwordInput"]').send_keys(str(password))
+
+driver.find_element_by_xpath('//*[@id="PageContentPlaceholder_loginControl_LoginButton"]').click()
+
+driver.get("https://ubc.ca.panopto.com/Panopto/Pages/Sessions/List.aspx#status=%5B1%5D&maxResults=1450&page=0")
+
+# Reaches 'Scheduled" tab here
+
+print("I'm done")
+
 # driver.close()
